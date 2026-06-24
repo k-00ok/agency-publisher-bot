b1645c604097719570e26ea276bc2ce104c5fa1d
k-00ok <kgg3dd@gmail.com>
2026-06-25 00:50:35 +0300

    Add Post dataclass model

--- app/models.py
+++ app/models.py
@@ -5,3 +5,13 @@ from dataclasses import dataclass
 class Client:
     id: int
     name: str
+
+
+@dataclass(slots=True)
+class Post:
+    id: int
+    client_id: int
+    media_type: str
+    media_paths: str
+    caption: str
+    status: str
